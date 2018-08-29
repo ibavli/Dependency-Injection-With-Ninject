83435cc8f4df0e3cbc4d9c91479891cd1ab7ba31
ibavli <ibrahim.bavli35@gmail.com>
2018-08-29 03:44:23 +0300

    Last uploads before Ninject. (Created HomeController, IndexPage)

---
+++ DependencyInjectionWithNinject.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using DependencyInjectionWithNinject.Dal.Abstract;
+using DependencyInjectionWithNinject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DependencyInjectionWithNinject.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        IAdminDal _adminDal;
+        public HomeController(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+        public ActionResult Index()
+        {
+            Admin admin = _adminDal.GetAdmin("admin");
+            if (admin == null)
+            {
+                Admin sampleAdmin = new Admin()
+                {
+                    UserName = "admin",
+                    Name = "ibrahim",
+                    Surname = "bavlı"
+                };
+                _adminDal.CreateAdmin(sampleAdmin);
+            }
+            return View(admin);
+        }
+    }
+}
